c359fb955310a65d0cd317fa26e3252c277077f3
Nick <NickHellemans93@gmail.com>
2019-07-19 11:28:26 +0200

    Add clubname and slogan to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,9 +4,26 @@ import { Link, prefixURL } from "next-prefixed";
 import PropTypes from "prop-types";
 
 class Header extends Component {
+  state = {
+    fading: false,
+    msg: "Quam parva nulli cedo"
+  };
+
   constructor() {
     super();
     this.onHamburgerClick = this.onHamburgerClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      const { msg } = this.state;
+      const messageToDisplay =
+        msg === "Quam parva nulli cedo"
+          ? "Al ben ik klein, ik wijk voor niets"
+          : "Quam parva nulli cedo";
+      this.setState({ msg: messageToDisplay });
+      this.setState({ fading: false });
+    }, 5000);
   }
 
   onHamburgerClick(e) {
@@ -17,10 +34,11 @@ class Header extends Component {
 
   render() {
     const { isMenuOpen } = this.props;
+    const { fading, msg } = this.state;
 
     return (
       <header className="z-20">
-        <div className="flex flex-row justify-between items-center px-4 py-6 border-b border-gray-200 sm:px-6 md:py-16 md:px-8 xl:px-16">
+        <div className="flex flex-row justify-between items-center px-4 py-12 border-b border-gray-200 sm:px-6 md:py-16 md:px-8 xl:px-16">
           <div className="relative z-500">
             <button
               onClick={this.onHamburgerClick}
@@ -38,27 +56,33 @@ class Header extends Component {
               <span className="hamburger-label">Menu</span>
             </button>
           </div>
-          <div className="logo">
+          <div className="logo text-center">
             <Link href="/">
-              <a title="K. Kontich F.C.">
+              <a title="K. Kontich F.C." className="flex items-center flex-col">
                 <img
-                  className="w-8 md:w-16 lg:w-18"
+                  className="w-8 md:w-12 lg:w-18"
                   src={prefixURL("/static/img/kkontichfc.svg")}
                   alt="K. Kontich F.C."
                   title="K. Kontich F.C."
                 />
-                <h1 className="hidden">K. Kontich F.C.</h1>
+                <h1 className="pt-2 pl-2 text-sm leading-tight xl:pl-0 lg:text-base">
+                  K. Kontich F.C.
+                  <br />
+                  <span className="normal-case font-normal italic text-gray-600 text-animate">
+                    {msg}
+                  </span>
+                </h1>
               </a>
             </Link>
           </div>
           <div className="hidden xl:block">
             <ul className="font-semibold text-gray-900 uppercase">
-              <li className="inline-block pr-12">
+              <li className="inline-block pr-10">
                 <Link href="/nieuws/overzicht/">
                   <a title="Nieuws">Nieuws</a>
                 </Link>
               </li>
-              <li className="inline-block pr-12">
+              <li className="inline-block pr-10">
                 <Link href="https://kontich.clubwereld.nl/">
                   <a title="KKFC Webshop" target="_blank">
                     KKFC Webshop
